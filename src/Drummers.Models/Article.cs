ff6c36223b1df073f70a879286868ed9f18b1444
Daniel Nikolov <horwits1@hotmail.com>
2017-03-03 15:02:45 +0200

    Added Drummers.Models project + News Article model

---
+++ src/Drummers.Models/Article.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using Drummers.Models.Contracts;
+
+namespace Drummers.Models
+{
+    public class Article : IArticle
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(400)]
+        public string Description { get; set; }
+    }
+}
---
+++ src/Drummers.Models/Contracts/IArticle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Drummers.Models.Contracts
+{
+    public interface IArticle
+    {
+        Guid Id { get; set; }
+
+        string Provider { get; set; }
+
+        string Url { get; set; }
+
+        string Title { get; set; }
+
+        string Description { get; set; }
+    }
+}
