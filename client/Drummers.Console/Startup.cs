3ac7b74100bcf3a2ab894b6b92ca8c9aca394f8e
Daniel Nikolov <horwits1@hotmail.com>
2017-03-03 15:38:05 +0200

    Added Console client

---
+++ client/Drummers.Console/Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace Drummers.Console
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Wellcome!");
+        }
+    }
+}
