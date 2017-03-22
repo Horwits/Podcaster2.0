e150cb70564b87aefaa7d80b3106ea15a3f3e8b0
Daniel Nikolov <horwits1@hotmail.com>
2017-03-22 16:07:31 +0200

    Added unit test for UserSetters + BaseTestClass.cs and AutoFixture

---
+++ src/Podcaster.UnitTests/BaseTestClass.cs
@@ -0,0 +1,15 @@
+﻿using Ploeh.AutoFixture;
+
+namespace Podcaster.UnitTests
+{
+    public abstract class BaseTestClass
+    {
+        public Fixture Fixture
+        {
+            get
+            {
+                return new Fixture();
+            }
+        }
+    }
+}
