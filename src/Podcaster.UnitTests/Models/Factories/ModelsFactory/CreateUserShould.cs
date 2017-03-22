4f3bdf03d3929b69d87bc91fbeb98c23b0b273e8
Daniel Nikolov <horwits1@hotmail.com>
2017-03-22 15:09:40 +0200

    Added Unit tests for Factories/ModelsFactory

---
+++ src/Podcaster.UnitTests/Models/Factories/ModelsFactory/CreateUserShould.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+using Podcaster.Models.Contracts;
+
+namespace Podcaster.UnitTests.Models.Factories.ModelsFactory
+{
+    [TestFixture]
+    public class CreateUserShould
+    {
+        [Test]
+        public static void Create_AnInstanceOf_IUser_WhenIsCalled()
+        {
+            // Arrange
+            var modelsFactory = new Podcaster.Models.Factories.ModelsFactory();
+
+            // Act
+            var user = modelsFactory.CreateUser();
+
+            // Assert
+            Assert.IsInstanceOf<IUser>(user);
+        }
+    }
+}
