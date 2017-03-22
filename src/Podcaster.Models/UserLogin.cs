910d07c0a7100b064d24a32ccc68676c9fbb274d
Daniel Nikolov <horwits1@hotmail.com>
2017-03-22 18:38:53 +0200

    Added interface for UserLogin

--- src/Podcaster.Models/UserLogin.cs
+++ src/Podcaster.Models/UserLogin.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 
+using Podcaster.Models.Contracts;
+
 namespace Podcaster.Models
 {
-    public class UserLogin
+    public class UserLogin : IUserLogin
     {
         public string LoginProvider { get; set; }
 
         public string ProviderKey { get; set; }
 
-        public User User { get; set; }
+        public IUser User { get; set; }
 
         public Guid UserId { get; set; }
     }
