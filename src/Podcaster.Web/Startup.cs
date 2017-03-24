0f2adb655ac421405332a71b6fcbdc3d172d8ab0
Daniel Nikolov <horwits1@hotmail.com>
2017-03-24 16:27:29 +0200

    Working changes

---
+++ src/Podcaster.Web/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+
+using Owin;
+
+[assembly: OwinStartup(typeof(Podcaster.Web.Startup))]
+
+namespace Podcaster.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            this.ConfigureAuth(app);
+        }
+    }
+}
