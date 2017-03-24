0a058fb9c248b7dbce6e6caec2345e2331597ed5
Daniel Nikolov <horwits1@hotmail.com>
2017-03-24 16:30:24 +0200

    All identity related models and services

--- src/Podcaster.Identity/ApplicationSignInManager.cs
+++ src/Podcaster.Identity/ApplicationSignInManager.cs
@@ -1,19 +1,17 @@
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 using Microsoft.Owin.Security;
 
 using Podcaster.Models;
-using Podcaster.Models.Contracts;
 
 namespace Podcaster.Identity
 {
-    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>, IApplicationSignInManager
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
     {
-        public ApplicationSignInManager(
-            ApplicationUserManager userManager,
-            IAuthenticationManager authenticationManager)
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
             : base(userManager, authenticationManager)
         {
         }
@@ -23,14 +21,9 @@ namespace Podcaster.Identity
             return user.GenerateUserIdentityAsync((ApplicationUserManager)this.UserManager);
         }
 
-        public virtual SignInStatus PasswordSignIn(string email, string password, bool isPersistent)
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
         {
-            return this.PasswordSignIn(email, password, isPersistent, true);
-        }
-
-        public virtual void SignIn(ApplicationUser user, bool isPersistent, bool rememberBrowser)
-        {
-            SignInManagerExtensions.SignIn(this, user, isPersistent, rememberBrowser);
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
         }
     }
 }
--- src/Podcaster.Identity/ApplicationUserManager.cs
+++ src/Podcaster.Identity/ApplicationUserManager.cs
@@ -1,63 +1,70 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System;
 
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 
+using Podcaster.Data.DbContexts;
 using Podcaster.Models;
-using Podcaster.Models.Contracts;
 
 namespace Podcaster.Identity
 {
-    public class ApplicationUserManager : UserManager<ApplicationUser>, IApplicationUserManager
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+    public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
             : base(store)
         {
         }
 
-        public virtual IdentityResult AddPassword(string userId, string password)
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            return UserManagerExtensions.AddPassword(this, userId, password);
-        }
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<PodcasterDbContext>()));
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
 
-        public virtual IdentityResult ChangePassword(string userId, string currentPassword, string newPassword)
-        {
-            return UserManagerExtensions.ChangePassword(this, userId, currentPassword, newPassword);
-        }
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                /*RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,*/
+            };
 
-        public virtual IdentityResult ConfirmEmail(string userId, string token)
-        {
-            return UserManagerExtensions.ConfirmEmail(this, userId, token);
-        }
+            // Configure user lockout defaults
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
 
-        public virtual IdentityResult Create(ApplicationUser user, string password)
-        {
-            return UserManagerExtensions.Create(this, user, password);
-        }
-
-        public virtual ClaimsIdentity CreateIdentity(ApplicationUser user, string authenticationType)
-        {
-            return UserManagerExtensions.CreateIdentity(this, user, authenticationType);
-        }
-
-        public virtual ApplicationUser FindById(string userId)
-        {
-            return UserManagerExtensions.FindById(this, userId);
-        }
-
-        public virtual ApplicationUser FindByName(string email)
-        {
-            return UserManagerExtensions.FindByName(this, email);
-        }
-
-        public virtual bool HasPassword(string userId)
-        {
-            return UserManagerExtensions.HasPassword(this, userId);
-        }
-
-        public virtual bool IsEmailConfirmed(string userId)
-        {
-            return UserManagerExtensions.IsEmailConfirmed(this, userId);
+            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
+            // You can write your own provider and plug it in here.
+            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
+            {
+                MessageFormat = "Your security code is {0}"
+            });
+            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
+            {
+                Subject = "Security Code",
+                BodyFormat = "Your security code is {0}"
+            });
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = 
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
         }
     }
+
+    // Configure the application sign-in manager which is used in this application.
 }
