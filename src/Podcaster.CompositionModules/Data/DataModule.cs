e0c842f26e1aa211909a0149a22b8e2d4914682a
Daniel Nikolov <horwits1@hotmail.com>
2017-03-25 22:00:38 +0200

    Added composition module for the Data layer

---
+++ src/Podcaster.CompositionModules/Data/DataModule.cs
@@ -0,0 +1,15 @@
+﻿using Ninject.Extensions.Conventions;
+using Ninject.Modules;
+
+using Podcaster.Data.Repositories.Contracts;
+
+namespace Podcaster.CompositionModules.Data
+{
+    public class DataModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind(x => x.FromAssemblyContaining<IRepository<object>>().SelectAllClasses().BindAllInterfaces());
+        }
+    }
+}
